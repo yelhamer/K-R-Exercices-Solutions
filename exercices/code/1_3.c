5d627cc3c75b8243a1d91f2d70a6579e15536df6
yelhamer <yelhamer@no-reply.github.com>
2020-02-12 01:32:12 +0100

    pt. 2

---
+++ exercices/code/1_3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*print Fahrenheit-Celsius table
+ * for fahr = 0, 20, ..., 300; floating-point version */
+int main(){
+	float fahr, celsius;
+	float lower, upper, step;
+	lower = 0;      /* lower limit of temperatuire scale */     
+	upper = 300;    /* upper limit */     
+	step = 20;      /* step size */     
+	fahr = lower;     
+	printf("Fahr\tCelsuis\n");
+	while(fahr <= upper){         
+		celsius = (5.0/9.0) * (fahr-32.0);         
+		printf("%4.0f\t%7.2f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+}
---
+++ exercices/code/1_4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*print Fahrenheit-Celsius table
+ * for fahr = 0, 20, ..., 300; floating-point version */
+int main(){
+	float fahr, celsius;
+	float lower, upper, step;
+	lower = -17.78;      /* lower limit of temperatuire scale */     
+	upper = 148.89;    /* upper limit */     
+	step = 11.11;      /* step size */     
+	celsius = lower;     
+	printf("Celsius\tFahr\n");
+	while(celsius <= upper){         
+		fahr = ((9.0/5.0) * celsius) + 32.01;         
+		printf("%7.2f\t%4.0f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+}
