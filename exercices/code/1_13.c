d74409c738302d6903b97a2d0f4bfdd8e02ce827
yelhamer <yelhamer@users.noreply.github.com>
2020-02-28 18:28:41 +0100

    1_13 Removed Junk

--- exercices/code/1_13.c
+++ exercices/code/1_13.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #define max 30
 
+
 int maxOccurance (int length[max]) {
 	int biggest = length[0];
 
@@ -11,13 +12,14 @@ int maxOccurance (int length[max]) {
 	return biggest;
 }
 
+
 void histoGram (int length[max]) {
 	int maxRow, currentLine, i;
 	
-	maxRow = maxOccurance(length); // maxOccurance -> 40 ; length -> x : x = length * 40 / maxOccurance
+	maxRow = maxOccurance(length);
 	printf("\n\nHistogram of the number of word-length occurances: \n");
+
 	for (currentLine = maxRow; currentLine >= 0; currentLine--) {
-		//a = currentLine * maxRow / 40;
 		if ( currentLine != 0) printf("\n%2d| ", currentLine);
 		else printf("\n  |");
 		for (i = 1; i < max; i++) {
@@ -27,30 +29,27 @@ void histoGram (int length[max]) {
 		}
 
 	}
-	//for (i = 0; i < (max * 3); i++)	printf("__");
+
 	printf("\n 0");
 	for (i = 1; i < max; i++) printf("%3d", i);
-
 }
+
 
 int main () {
 	int length[max], c, wordLengthCounter = 0;
 
 	for (int i = 0; i < max; i++) length[i] = 0;
+	printf("Input: ");
 	while ((c = getchar()) != EOF && c != '\n'){
 		if (c != ' ' && c != '\t') 
 			wordLengthCounter++;
 		else {
-			//wordLengthCounter++;
 			length[wordLengthCounter]++;
 			wordLengthCounter = 0;
 		}
 	}
+
 	length[wordLengthCounter]++;
-	//for (int i = 0; i < max; i++) printf("%d\n", length[i]);
-	//histoGram(length);
-	//for (int i = 0; i < max; i++) printf("Occurance of %d is: %d\n", i, length[i]);
-	//printf("max length: %d\n", maxOccurance(length));
 	histoGram(length);
 	printf("\n\n");
 }
